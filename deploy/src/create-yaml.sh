c58a5b7530d171f52e8959b77cabaa0f2cde509c
scrogatl <scott@scottandrewrogers.com>
2021-07-21 15:26:11 -0400

    Fetching upstream

--- deploy/src/create-yaml.sh
+++ deploy/src/create-yaml.sh
@@ -12,19 +12,29 @@ echo "    location: " $K8S_LOCATION
 echo "  repository: " $K8S_REPOSITORY
 
 for f in *.yaml ; do
+<<<<<<< HEAD
+=======
+	if 
+>>>>>>> 662bcb3 (Improved configuration of the app)
 	echo "creating ../$f"
 	envsubst < $f > ../$f
 done
 
+<<<<<<< HEAD
 if [ ! -d "../namespace/" ]
 then 
 	mkdir ../namespace
 fi
 for f in namespace/* ; do
+=======
+for f in namespace/* ; do
+	if 
+>>>>>>> 662bcb3 (Improved configuration of the app)
 	echo "creating ../$f"
 	envsubst < $f > ../$f
 done
 
+<<<<<<< HEAD
 if [ ! -d "../services/" ]
 then 
 	mkdir ../services
@@ -37,3 +47,33 @@ done
 
 #echo "create ../../payments/src/Payments/applicationTags.yaml"
 #envsubst < apptags/applicationTags.yaml > ../../payments/src/Payments/applicationTags.yaml
+=======
+for f in services/* ; do
+	if 
+	echo "creating ../$f)"
+	envsubst < $f > ../$f
+done
+
+declare -a services=("notification"
+                     "packaging"
+					 "printing"
+					 "payments"
+					 "warehouse"
+					 "delivery"
+					 "styling"
+					 "shopping")
+for svc in "${services[@]}"
+do
+ echo export SERVICE=$svc
+ export SERVICE=$svc
+ if [ "$svc" = "payments" ]
+ then
+	echo "create ../../$svc/src/Payments/applicationTags.yaml"
+    envsubst < apptags/applicationTags.yaml > ../../$svc/src/Payments/applicationTags.yaml
+ else 
+	echo "creating ../../$svc/applicationTags.yaml"
+    envsubst < apptags/applicationTags.yaml > ../../$svc/applicationTags.yaml 
+ fi
+ 
+done 
+>>>>>>> 662bcb3 (Improved configuration of the app)
