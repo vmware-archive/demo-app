a16e7aaedbd6c8868962f7eaf468a36d9e337b48
scrogatl <scott@scottandrewrogers.com>
2021-10-04 10:47:05 -0400

    Improved build & deploy - secret now created from file

--- images/build.sh
+++ images/build.sh
@@ -1,18 +1,34 @@
 #! /bin/zsh
 
-source ../deploy/src/values.sh
+if [[ -f ../deploy/src/values.sh ]]; then 
+     	source ../deploy/src/values.sh
+fi
+
+if [ -z ${K8S_REPOSITORY+x} ]; then 
+        echo "\n\nusage: export K8S_REPOSITORY=[your private registry here]\n\n"
+        exit
+fi
+
+if [ -z ${1+x} ]; then 
+        echo "\n\nusage: ./build.sh [directory]\n\n"
+        exit
+fi
 
 cd $1
 
+
+IMAGE=$(echo $1 | sed 's:/*$::')
+echo "\n>>> Creating image \"$IMAGE\" and pushing to \"$K8S_REPOSITORY/$IMAGE\"\n"
+
 if [[ -f prepare.sh ]]; then
-	./prepare.sh
+	./prepare.sh 
 fi
 
-docker build . -t $K8S_REPOSITORY/$1
-docker push $K8S_REPOSITORY/$1
+docker build . -t $K8S_REPOSITORY/$IMAGE --build-arg JAR=$IMAGE
+docker push $K8S_REPOSITORY/$IMAGE
 
 if [[ -f clean.sh ]]; then
-	./clean.sh
+	./clean.sh 
 fi
 
 
