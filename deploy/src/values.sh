64a858ad537780e54211176d67b08f6772de756a
scrogatl <scott@scottandrewrogers.com>
2021-10-25 10:58:43 -0400

    updated repo with public repo

--- deploy/src/values.sh
+++ deploy/src/values.sh
@@ -3,5 +3,5 @@ export K8S_APPLICATION=tacocat
 export K8S_CLUSTER=cluster1
 export K8S_LOCATION=americas
 export K8S_ENVIRONMENT=dev
-export K8S_REPOSITORY=public.ecr.aws/r8e5f6o2/to-demo
+export K8S_REPOSITORY=public.ecr.aws/tanzu_observability_demo_app/to-demo
 export WAVEFRONT_BASE64_TOKEN=<YOUR BASE64 ENCODED TOKEN HERE>
