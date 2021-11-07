cc5bf5c52b015c85519b3b9d658f427ade9bb709
Scott Rogers <scottr@pop-os.localdomain>
2021-11-07 15:11:42 -0500

    Initial re-configure for docker or TAS (CF/PCF)w!

---
+++ images-docker-cf/inventory/clean.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+rm inventory
+rm inventory.conf
---
+++ images-docker-cf/payments/prepare.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+source ../../deploy/src/values.sh
+cp -r ../../payments .
+envsubst <  ../../deploy/src/data/$1-app.yaml > $1-app.yaml
+envsubst < ../../deploy/src/data/$1-app-tags.yaml > $1-app-tags.yaml 
