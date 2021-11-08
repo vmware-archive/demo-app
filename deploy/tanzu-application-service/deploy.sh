e78453266bbbfd23d86a28dd4811344aa3f8b6d4
Scott Rogers <rogerssc@vmware.com>
2021-11-08 11:21:21 -0500

    TAS (pcf/cf) push script with docker containers

--- deploy/tanzu-application-service/deploy.sh
+++ deploy/tanzu-application-service/deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+IMAGE='NONE'
 
 if [[ -f ../../deploy/src/values.sh ]]; then 
      	source ../../deploy/src/values.sh
@@ -8,29 +9,21 @@ else
 fi
 
 if [ -z ${K8S_REPOSITORY+x} ]; then 
-        echo "nusage: export K8S_REPOSITORY=[your private registry here]"
+        echo "usage: export K8S_REPOSITORY=[your private registry here]"
         exit
 fi
 
-cf stop delivery-service
-cf stop inventory-service
-cf stop loadgen-service 
-cf stop notification-service 
-cf stop packaging-service 
-cf stop payments-service 
-cf stop printing-service 
-cf stop styling-service 
-cf stop warehouse-service 
-cf stop shopping-service 
-	
-cf push delivery-service --docker-image $K8S_REPOSITORY/delivery:latest --no-manifest --health-check-type process --no-start
-cf push inventory-service --docker-image $K8S_REPOSITORY/inventory:latest --no-manifest --health-check-type process --no-start
-cf push loadgen-service --docker-image $K8S_REPOSITORY/loadgen:latest --no-manifest --health-check-type process --no-start
-cf push notification-service --docker-image $K8S_REPOSITORY/notification:latest --no-manifest --health-check-type process --no-start
-cf push packaging-service --docker-image $K8S_REPOSITORY/packaging:latest --no-manifest --health-check-type process --no-start
-cf push payments-service --docker-image $K8S_REPOSITORY/payments:latest --no-manifest --health-check-type process --no-start
-cf push printing-service --docker-image $K8S_REPOSITORY/printing:latest --no-manifest --health-check-type process --no-start
-cf push styling-service --docker-image $K8S_REPOSITORY/styling:latest --no-manifest --health-check-type process --no-start
-cf push warehouse-service --docker-image $K8S_REPOSITORY/warehouse:latest --no-manifest --health-check-type process --no-start
-cf push shopping-service --docker-image $K8S_REPOSITORY/shopping:latest --no-manifest --health-check-type process --no-start
-	
+deploy_service() {
+        echo "deploying $IMAGE"
+        cf push $IMAGE-service --docker-image $K8S_REPOSITORY/$IMAGE:latest --no-manifest --health-check-type process --no-start
+        cf map-route $IMAGE-service apps.internal --hostname $IMAGE-service
+        # cf start $IMAGE-service
+}
+
+for f in ../../images-docker-cf/*; do
+	if [ -d "$f" ]; then
+                IMAGE=$(basename $f)
+                deploy_service
+		echo 
+	fi 
+done
