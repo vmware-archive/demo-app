59ed7bbf0f412474044aea0dfe8eb56d56325387
Scott Rogers <scott@scottandrewrogers.com>
2021-11-12 08:34:29 -0500

    Refactored wf-config.yaml

--- images-docker-cf/notification/prepare.sh
+++ images-docker-cf/notification/prepare.sh
@@ -4,4 +4,4 @@ source ../../deploy/src/values.sh
 cp ../../$1/target/$1-*.*.*.jar $1.jar
 envsubst <  ../../deploy/src/data/$1-app.yaml > $1-app.yaml
 envsubst < ../../deploy/src/data/$1-app-tags.yaml > $1-app-tags.yaml 
-#envsubst < ../../deploy/src/data/wf-config.yaml > wf-config.yaml
+envsubst < ../../deploy/src/data/wf-config.yaml > wf-config.yaml
