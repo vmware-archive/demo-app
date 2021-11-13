5228a4214dd71ee9a7089fac7ca9ead16bc4212f
Scott Rogers <scott@scottandrewrogers.com>
2021-11-13 09:10:26 -0500

    Further TAS updates

--- images-docker-cf/warehouse/prepare.sh
+++ images-docker-cf/warehouse/prepare.sh
@@ -3,3 +3,4 @@
 cp -r ../../warehouse .
 envsubst <  ../../deploy/src/data/$1-app.yaml > $1-app.yaml
 envsubst < ../../deploy/src/data/$1-app-tags.yaml > $1-app-tags.yaml 
+envsubst < ../../deploy/src/data/wf-config.yaml > wf-config.yaml
