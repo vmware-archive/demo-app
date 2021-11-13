abb5295b437ff69daf7678190e2715f2e54af4dd
Scott Rogers <rogerssc@vmware.com>
2021-11-13 09:11:22 -0500

    add --no-start and --no-route to cf push

--- deploy/tanzu-application-service/deploy.sh
+++ deploy/tanzu-application-service/deploy.sh
@@ -15,9 +15,9 @@ fi
 
 deploy_service() {
         echo "deploying $IMAGE"
-        cf push $IMAGE-service --docker-image $K8S_REPOSITORY/$IMAGE:latest --no-manifest --health-check-type process --no-start
-        cf map-route $IMAGE-service apps.internal --hostname $IMAGE-service
-        # cf start $IMAGE-service
+        cf push $IMAGE-service --docker-image $K8S_REPOSITORY/$IMAGE:latest --no-manifest --health-check-type process --no-start --no-start --no-route
+        cf map-route $IMAGE-service apps.internal --hostname $IMAGE-service 
+        #cf start $IMAGE-service
 }
 
 for f in ../../images-docker-cf/*; do
@@ -27,3 +27,16 @@ for f in ../../images-docker-cf/*; do
 		echo 
 	fi 
 done
+
+
+cf start printing-service
+cf start packaging-service
+cf start notification-service
+cf start delivery-service
+cf start warehouse-service
+cf start payments-service
+cf start inventory-service
+cf start styling-service
+cf start shopping-service
+cf start loadgen-service
+cf scale -i 5 loadgen-service
