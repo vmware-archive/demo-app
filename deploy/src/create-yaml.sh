35868737b5b83c9fb73490cd8fc70bf5feff0b4d
Scott Rogers <rogerssc@vmware.com>
2022-01-19 09:08:57 -0500

    continue merging upstream

--- deploy/src/create-yaml.sh
+++ deploy/src/create-yaml.sh
@@ -12,41 +12,19 @@ echo "    location: " $K8S_LOCATION
 echo "  repository: " $K8S_REPOSITORY
 
 for f in *.yaml ; do
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-	if 
->>>>>>> 662bcb3 (Improved configuration of the app)
->>>>>>> wavefrontHQ-master
 	echo "creating ../$f"
 	envsubst < $f > ../$f
 done
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> wavefrontHQ-master
 if [ ! -d "../namespace/" ]
 then 
 	mkdir ../namespace
 fi
 for f in namespace/* ; do
-<<<<<<< HEAD
-=======
-=======
-for f in namespace/* ; do
-	if 
->>>>>>> 662bcb3 (Improved configuration of the app)
->>>>>>> wavefrontHQ-master
 	echo "creating ../$f"
 	envsubst < $f > ../$f
 done
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> wavefrontHQ-master
 if [ ! -d "../services/" ]
 then 
 	mkdir ../services
@@ -54,48 +32,8 @@ fi
 for f in services/* ; do
 	if 
 	echo "creating ../$f"
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> wavefrontHQ-master
 	envsubst < $f > ../$f
 done
 
 #echo "create ../../payments/src/Payments/applicationTags.yaml"
 #envsubst < apptags/applicationTags.yaml > ../../payments/src/Payments/applicationTags.yaml
-<<<<<<< HEAD
-=======
-=======
-for f in services/* ; do
-	if 
-	echo "creating ../$f)"
-=======
->>>>>>> bac6e66 (Improved build & deploy - secret now created from file)
-	envsubst < $f > ../$f
-done
-
-declare -a services=("notification"
-                     "packaging" 
-	             "printing"
-                     "payments"
-                     "warehouse"
-                     "delivery"
-                     "styling"
-                     "shopping")
-
-for svc in "${services[@]}"
-do
- echo export SERVICE=$svc
- export SERVICE=$svc
- if [ "$svc" = "payments" ]
- then
-	echo "create ../../$svc/src/Payments/applicationTags.yaml"
-    envsubst < apptags/applicationTags.yaml > ../../$svc/src/Payments/applicationTags.yaml
- else 
-	echo "creating ../../$svc/applicationTags.yaml"
-    envsubst < apptags/applicationTags.yaml > ../../$svc/applicationTags.yaml 
- fi
- 
-done 
->>>>>>> 662bcb3 (Improved configuration of the app)
->>>>>>> wavefrontHQ-master
